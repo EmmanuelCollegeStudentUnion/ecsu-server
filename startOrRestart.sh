5d2d3c6859932185787b767b1f97faf58ed20ad3
Nathan Hawkes <nh487@pip.srcf.societies.cam.ac.uk>
2019-01-25 14:47:16 +0000

    Config fixes

--- startOrRestart.sh
+++ startOrRestart.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-ssh nh487@sinkhole.srcf.net -c "cd $PWD; ~/.local/bin/yarn prod"
+export PATH=/.local/bin:$PATH
+ssh nh487@sinkhole.srcf.net -C "cd $PWD;export PATH=/.local/bin:$PATH&&~/.local/bin/yarn prod"
