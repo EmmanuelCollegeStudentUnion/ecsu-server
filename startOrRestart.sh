9c0c8cdce697b2519ec56489598fc4b3118803a4
nh487 <nh487@cam.ac.uk>
2019-01-25 15:12:59 +0000

    Config fix

--- startOrRestart.sh
+++ startOrRestart.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-export PATH=/.local/bin:$PATH
-ssh nh487@sinkhole.srcf.net -C "cd $PWD;export PATH=/.local/bin:$PATH&&~/.local/bin/yarn prod"
+export PATH=~/.local/bin:$PATH
+ssh nh487@sinkhole.srcf.net -C "cd $PWD;export PATH=~/.local/bin:$PATH&&~/.local/bin/yarn prod"
