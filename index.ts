19f781282ce27cd00165e26364c50b8a970c7e8a
nh487 <nh487@cam.ac.uk>
2019-01-26 02:57:06 +0000

    CORS

--- index.ts
+++ index.ts
@@ -233,6 +233,12 @@ const server = new ApolloServer({
 
 const app = express();
 
+//CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 passport.use(new RavenStrategy({
   desc: 'ECSU',
