5c010ffa81588127518d9faa3655113fb115bd97
nh487 <nh487@cam.ac.uk>
2019-03-11 01:14:28 +0000

    cwd fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'ecsu-server',
     script: 'dist/index.js',
-    cwd: `${__dirname}`,
+    cwd: `${process.env.PWD}`,
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     env: {
