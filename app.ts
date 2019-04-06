32753b1814a4e0558292e48c6e39905a5226ce8d
nh487 <nh487@cam.ac.uk>
2019-04-06 16:53:07 +0100

    Server running page

---
+++ app.ts
@@ -0,0 +1,38 @@
+require('dotenv').config()
+import express from 'express';
+import image from './image';
+import applyMinutesMiddleware, { minutes } from './minutes';
+import applyGraphqlMiddleware from './schema';
+import applyRssFeedMiddleware from './rssfeed';
+import applyAuthMiddleware from './auth';
+
+
+const app = express();
+
+//CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (_req, res) => res.send('Server running.'))
+
+applyAuthMiddleware(app);
+applyMinutesMiddleware(app);
+applyGraphqlMiddleware(app)
+applyRssFeedMiddleware(app);
+
+app.get('/image/:folder/:file(*)', (req, res, next) => {
+    res.type('image/png');
+    const stream = image(`./assets/images/${req.params.folder}/${req.params.file}`, 0, 0, 0);
+    stream.on('error', next).pipe(res);
+})
+
+app.get('/user_uploads/room_database/:folder/:file(*)', (req, res, next) => {
+    res.type('image/png');
+    const stream = image(`./user_uploads/room_database/${req.params.folder}/${req.params.file}`, 0, 0, 0);
+    stream.on('error', next).pipe(res);
+})
+
+export default app;
--- index.ts
+++ index.ts
@@ -1,41 +1,4 @@
-
-require('dotenv').config()
-import express from 'express';
-import image from './image';
-import applyMinutesMiddleware, { minutes } from './minutes';
-import applyGraphqlMiddleware from './schema';
-import applyRssFeedMiddleware from './rssfeed';
-import applyAuthMiddleware from './auth';
-
-
-const app = express();
-
-//CORS
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
-
-applyAuthMiddleware(app);
-applyMinutesMiddleware(app);
-applyGraphqlMiddleware(app)
-applyRssFeedMiddleware(app);
-
-app.get('/image/:folder/:file(*)', (req, res, next) => {
-  res.type('image/png');
-  const stream = image(`./assets/images/${req.params.folder}/${req.params.file}`, 0, 0, 0);
-  stream.on('error', next).pipe(res);
-})
-
-app.get('/user_uploads/room_database/:folder/:file(*)', (req, res, next) => {
-  res.type('image/png');
-  const stream = image(`./user_uploads/room_database/${req.params.folder}/${req.params.file}`, 0, 0, 0);
-  stream.on('error', next).pipe(res);
-})
-
+import app from "./app";
 
 app.listen({ port: process.env.PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:3254`)
