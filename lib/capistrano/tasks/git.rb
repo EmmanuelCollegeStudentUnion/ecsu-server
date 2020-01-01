d76c1feba6d91207fdacd65c72d3aa848355a301
Ben Shute <btfs2@cam.ac.uk>
2020-01-01 19:45:47 +0000

    attempt to fix capistrano git thing

---
+++ lib/capistrano/tasks/git.rb
@@ -0,0 +1,15 @@
+# Taken from https://gist.github.com/corny/7459729
+# Thanks corny
+
+namespace :git do
+    desc 'Copy repo to releases'
+    task create_release: :'git:update' do
+      on roles(:all) do
+        with fetch(:git_environmental_variables) do
+          within repo_path do
+            execute :git, :clone, '-b', fetch(:branch), '--recursive', '.', release_path
+          end
+        end
+      end
+    end
+  end
