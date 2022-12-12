ba417ed0b0731439e860169748e1aadc82370801
JamesSteiner <jamessteiner2002@outlook.com>
2022-12-12 09:19:11 +0000

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "webserver.srcf.net", user: "jw2117", roles: %w{app}
+server "webserver.srcf.net", user: "jas330", roles: %w{app}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
